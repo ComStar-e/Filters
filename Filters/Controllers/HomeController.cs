0c4fac6c885b1dc1948806184ebc0f8eb4ff57b5
Vitaliy Brichlov <brichkovv@gmail.com>
2019-07-05 15:27:25 +0300

    Add project files.

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Filters.Infrastructure;
+
+
+namespace Filters.Controllers
+{
+    public class HomeController :Controller
+    {
+        [HttpsOnly]
+        public IActionResult Index() => View("Message", "This HTTPS Message Only");
+        
+        
+    }
+}
